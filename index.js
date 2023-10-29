80914b76f5f1347013f2f1c3d302d0ff7344d37f
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-10-29 23:05:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+// document.querySelector("Button").addEventListener("click",handleClicked);
+// // in the above line we called the function without paranthesis handleClicked because if we use paranthesis 
+// // then the function will be like already clicked but here we want it to happen only when we click the button
+// // i.e., quarySelector...
+// function handleClicked(){
+//     alert("i got clicked");
+// }
+
+// or we can put function on the first line
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+//  for detecting key
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+function makeSound(key){
+    // var key = this.innerHTML;
+    switch (key) {
+        case "w":
+        var tom1= new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+        case "a":
+        var tom2= new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+        case "s":
+        var tom3= new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+        case "d":
+        var tom4= new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+        case "l":
+        var crash= new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+        case "k":
+        var kick= new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+        case "j":
+        var snare= new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+    
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
